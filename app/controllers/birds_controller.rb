14d6d23e9b77a1f6105fc9c8a337db9d64fb7911
Alina Meyer <alina.eichmatt@gmail.com>
2026-01-27 20:18:24 +0100

    Add endpoint to create bird

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,4 +6,22 @@ class BirdsController < ApplicationController
   def show
     @bird = Bird.find(params[:id])
   end
+
+  def new
+    @bird = Bird.new
+  end
+
+  def create
+    @bird = Bird.new(bird_params)
+    if @bird.save # validate, save if success
+      redirect_to @bird
+    end # else, back to form with entered info
+  end
+
+  # This filters the incoming param and make sure there is bird array with param name
+  # other obj are ignored
+  private
+    def bird_params
+      params.expect(bird: [ :name])
+    end
 end
